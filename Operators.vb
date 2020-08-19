8083da8f5db59368a3663f825634d969af457e91
cameron coxen <coxecame@isu.edu>
2020-08-19 08:44:41 -0600

    Add project files.

---
+++ Operators.vb
@@ -0,0 +1,34 @@
+﻿Module Operators
+    'Calvin Coxen
+    'RCET0265
+    'Fall 2020
+    'Smooth Operators
+    '
+
+    Sub Main()
+
+        'Time for LE maths
+
+        Console.WriteLine(2 + 2)
+        Console.ReadLine()
+
+        Console.WriteLine(2 / 7)
+        Console.ReadLine()
+
+        Console.WriteLine(82 - 2)
+        Console.ReadLine()
+
+        Console.WriteLine(2 * 3)
+        Console.ReadLine()
+
+        Console.WriteLine(2 \ 7)
+        Console.ReadLine()
+
+        Console.WriteLine(36.7 Mod 2.8)
+        Console.ReadLine()
+
+        Console.WriteLine(2 ^ 3)
+        Console.ReadLine()
+    End Sub
+
+End Module
